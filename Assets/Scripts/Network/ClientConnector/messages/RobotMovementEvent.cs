551ad80c0e123f26c583a44dd56442459668e090
Cody Huzarski <chuzarski@gmail.com>
2022-04-03 19:46:31 -0400

    Added support for robot movement updates

---
+++ Assets/Scripts/Network/ClientConnector/messages/RobotMovementEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ClientConnector.messages
+{
+    public class RobotMovementEvent
+    {
+        [JsonPropertyName("player_id")]
+        public string PlayerId {get; set;}
+        [JsonPropertyName("robot_id")]
+        public string RobotId { get; set; }
+        [JsonPropertyName("x")]
+        public int X { get; set; }
+        [JsonPropertyName("y")]
+        public int Y { get; set; }
+  
+    }
+}
