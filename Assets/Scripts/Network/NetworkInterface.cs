2001a6389e57181e8a0cee6a7fe71e5f361e8025
AndersenBall <andersen.ball@gmail.com>
2022-04-24 16:00:02 -0400

    fixed issues from merge

--- Assets/Scripts/Network/NetworkInterface.cs
+++ Assets/Scripts/Network/NetworkInterface.cs
@@ -113,8 +113,8 @@ public class NetworkInterface : MonoBehaviour
     private void MoveRobotPosition(ICarrierPigeon carrier)
     {
         RobotMovementEvent movementEvent = PayloadExtractor.GetRobotMovementEvent(carrier);
-        Debug.Log($"Player \"{movementEvent.PlayerId}\" Robot \"{movementEvent.RobotId}\"" +
-            $" moved to -> X: {movementEvent.X} Y: {movementEvent.Y} sector: {movementEvent.NewLocation.SectorId}");
+        //Debug.Log($"Player \"{movementEvent.PlayerId}\" Robot \"{movementEvent.RobotId}\"" +
+        //    $" moved to -> X: {movementEvent.X} Y: {movementEvent.Y} sector: {movementEvent.NewLocation.SectorId}");
 
         string sectorID = movementEvent.NewLocation.SectorId;
         robotMaster.MoveRobot(movementEvent.RobotId, (movementEvent.X, movementEvent.Y), sectorID);
