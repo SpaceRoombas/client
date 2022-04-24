4100284abc7b6f962abdde0c6c7cdc8959e672de
TessaPeruzzi <54461147+TessaPeruzzi@users.noreply.github.com>
2022-04-24 12:24:16 -0400

    Update code editor

--- Assets/Scripts/UIScripts/RobotViewButton.cs
+++ Assets/Scripts/UIScripts/RobotViewButton.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RobotViewButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string name;
+    public string firmware;
+    public TMP_InputField input;
+    
+    public void MovetoCodeEditor()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject r = GameObject.Find("Robots/" + name);
+        r.GetComponent<TMP_InputField>().text = firmware;
     }
 }
