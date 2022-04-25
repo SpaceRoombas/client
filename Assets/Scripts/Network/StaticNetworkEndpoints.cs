5a60d7b9b6d1e20d45e94dd03b09fba8ef372474
Cody Huzarski <chuzarski@gmail.com>
2022-04-24 22:26:48 -0400

    Changes to endpoint addresses

--- Assets/Scripts/Network/StaticNetworkEndpoints.cs
+++ Assets/Scripts/Network/StaticNetworkEndpoints.cs
@@ -8,9 +8,9 @@ namespace Network.Statics
 {
     public class StaticNetworkEndpoints
     {
-        public const string LOGIN_ENDPOINT = "http://localhost:9000/login";
-        public const string JOIN_ENDPOINT = "http://localhost:9000/joinmatch";
-        public const string LISTING_ENDPOINT = "http://localhost:9000/fetchmatches";
-        public const string REGISTER_ENDPOINT = "http://localhost:9000/register";
+        public const string LOGIN_ENDPOINT = "http://34.69.124.214:9000/login";
+        public const string JOIN_ENDPOINT = "http://34.69.124.214:9000/joinmatch";
+        public const string LISTING_ENDPOINT = "http://34.69.124.214:9000/fetchmatches";
+        public const string REGISTER_ENDPOINT = "http://34.69.124.214:9000/register";
     }
 }
